d9a3dee573207809701cfdedfda90ff539041e9c
Ashley Chou <ashchou3@gmail.com>
2012-12-09 20:29:41 -0500

    Implemented menu

---
+++ NewsReel/UIButton+NewsReelAdditions.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+NewsReelAdditions.h
+//  NewsReel
+//
+//  Created by Ashley Chou on 12/9/12.
+//  Copyright (c) 2012 Ashley Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (NewsReelAdditions)
+
+@end
---
+++ NewsReel/UIButton+NewsReelAdditions.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+NewsReelAdditions.m
+//  NewsReel
+//
+//  Created by Ashley Chou on 12/9/12.
+//  Copyright (c) 2012 Ashley Chou. All rights reserved.
+//
+
+#import "UIButton+NewsReelAdditions.h"
+
+@implementation UIButton (NewsReelAdditions)
+
+@end
